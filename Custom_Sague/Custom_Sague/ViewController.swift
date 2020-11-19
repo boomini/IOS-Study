e20dcda3608e8284001dd49591f872f100c8b62a
boomini <bomin2641@gmail.com>
2020-11-19 10:30:56 +0900

    unwind_segue

---
+++ Custom_Sague/Custom_Sague/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom_Sague
+//
+//  Created by EMGRAM on 2020/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func unwindMainVC(segue: UIStoryboardSegue){
+        
+    }
+    //스토리보드 상에서 코드와 연결시킬 수 있는 준비가 된 것이다.
+    //한번에 어느정도 이상으로 돌아가고싶을때
+    //쌓이는게 아니라
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
